763a2b3b32e298ab0bf3c3d5759d6634e9155f72
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2025-11-25 21:05:50 +1000

    12. Rock&Paper&Scissors and TodoList

--- 0. JS Basic/10. Rock-Paper-Scissors/scripts/10-rock-papaer-scissors.js
+++ 0. JS Basic/10. Rock-Paper-Scissors/scripts/10-rock-papaer-scissors.js
@@ -1,6 +1,79 @@
 let score = JSON.parse(localStorage.getItem('score')) || { wins: 0, losses: 0, ties: 0,};    
 
         updateScoreElement();
+       
+
+        document.querySelector('.js-rock-button').addEventListener('click', ()=>{
+            playGame('rock');});
+
+        document.querySelector('.js-paper-button').addEventListener('click', ()=>{
+            playGame('paper');});
+
+        document.querySelector('.js-scissors-button').addEventListener('click', ()=>{
+            playGame('scissors');});
+            
+        document.querySelector('.js-autoPlay-button').addEventListener('click', () => {
+            autoPlay();});
+        
+        document.querySelector('.js-reset-button').addEventListener('click', () => {
+            showResetConfirmation();});
+          
+
+        document.body.addEventListener('keydown', (event) => {
+            if (event.key === 'r') {playGame('rock');} 
+            else if (event.key === 'p') {playGame('paper');}
+            else if (event.key === 's') {playGame('scissors');}
+            else if (event.key === 'a') {autoPlay();}
+            else if (event.key === 'Backspace') {showResetConfirmation();}
+        });
+
+
+        function showResetConfirmation(){
+            document.querySelector('.js-reset-confirmation').innerHTML = `
+            Are you sure you want to reset the score? 
+            <button class="js-reset-confirm-yes reset-confirm-button">Yes</button>
+            <button class="js-reset-confirm-no reset-confirm-button">No</button>`;
+
+            document.querySelector('.js-reset-confirm-yes').addEventListener('click', () => {
+                resetScore();
+                hideResetConfirmation();
+            });
+            
+            document.querySelector('.js-reset-confirm-no').addEventListener('click', () => {
+                hideResetConfirmation();
+            });
+        }
+
+        function hideResetConfirmation(){
+            document.querySelector('.js-reset-confirmation').innerHTML = '';
+        }
+
+        function resetScore(){
+            score.wins = 0;
+            score.losses = 0;
+            score.ties = 0;
+            localStorage.removeItem('score');
+            updateScoreElement();
+
+        }
+
+        let isAutoPlaying = false;
+        let intervalID; 
+
+        function autoPlay(){
+            if (!isAutoPlaying){
+                intervalID = setInterval(function(){
+                        const playerMove = pickComputerMove();
+                        playGame(playerMove);
+                    }, 1000);
+                isAutoPlaying = true;
+                document.querySelector('.js-autoPlay-button').innerHTML = 'Stop Playing';
+            } else {
+                clearInterval(intervalID);
+                isAutoPlaying = false;
+                document.querySelector('.js-autoPlay-button').innerHTML = 'Auto Play';
+            }
+        }
 
         function playGame(playerMove){
 
