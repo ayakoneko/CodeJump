0b05018d032dbd8c9af669413dfdb37ff77599c2
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2025-11-27 08:26:18 +1000

    13. Amazon - Add new quantity input and save button for update with css styling

--- 0. JS Basic/13. Amazon-clone/scripts/checkout.js
+++ 0. JS Basic/13. Amazon-clone/scripts/checkout.js
@@ -37,8 +37,12 @@ cart.forEach((cartItem) => {
             <span>
               Quantity: <span class="quantity-label">${cartItem.quantity}</span>
             </span>
-            <span class="update-quantity-link link-primary">
+            <span class="update-quantity-link link-primary js-update-link" data-product-id="${matchingProduct.id}">
               Update
+            </span>
+            <input class="quantity-input">
+            <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}">
+              Save
             </span>
             <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
@@ -110,6 +114,24 @@ document.querySelectorAll('.js-delete-link')
       updateCartQuantity();
     });
   });
+
+
+document.querySelectorAll('.js-update-link').forEach((link) => {
+  link.addEventListener('click', () => {
+    const productId = link.dataset.productId;
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.classList.add("is-editing-quantity");
+  });
+});
+
+document.querySelectorAll('.js-save-link').forEach((link) => {
+  link.addEventListener('click', () => {
+    const productId = link.dataset.productId;
+    const container = document.querySelector(`.js-cart-item-container-${productId}`);
+    container.classList.remove("is-editing-quantity");
+  });
+});
+
 
 
 function updateCartQuantity(){
