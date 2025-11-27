672f87674c242726b507491121ff0053b665bd92
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2025-11-27 21:08:11 +1000

    13. Amazon - Make delivery options interactive

--- 0. JS Basic/13. Amazon-clone/scripts/checkout.js
+++ 0. JS Basic/13. Amazon-clone/scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart, calculateCartQuantity, updateQuantity} from '../data/cart.js';
+import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliveryOption} from '../data/cart.js';
 import {deliveryOptions} from '../data/deliverOptions.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
@@ -89,7 +89,8 @@ function deliveryOptionHTML(matchingProduct, cartItem){
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
     
     html += `
-      <div class="delivery-option">
+      <div class="delivery-option js-delivery-option" 
+        data-product-id="${matchingProduct.id}" data-delivery-option-id="${deliveryOption.id}">
         <input type="radio" 
         ${isChecked ? 'checked': ''}
         class="delivery-option-input"
@@ -107,6 +108,13 @@ function deliveryOptionHTML(matchingProduct, cartItem){
   });
   return html;
 }
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+  element.addEventListener('click', () => {
+    const {productId, deliveryOptionId} = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
+  })
+});
 
 
 document.querySelectorAll('.js-delete-link')
@@ -172,5 +180,3 @@ function updateCartQuantity(){
   document.querySelector('.js-return-to-home-link').innerHTML = `${cartQuantity} items`;
 }
 
-
-updateCartQuantity();
