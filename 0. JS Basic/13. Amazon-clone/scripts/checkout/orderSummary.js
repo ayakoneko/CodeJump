df96f0a4d641600cf99199d0ed482508ba743c69
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2025-11-28 12:06:22 +1000

    13. Amazon - minor fix

--- 0. JS Basic/13. Amazon-clone/scripts/checkout/orderSummary.js
+++ 0. JS Basic/13. Amazon-clone/scripts/checkout/orderSummary.js
@@ -2,7 +2,6 @@ import {cart, removeFromCart, calculateCartQuantity, updateQuantity, updateDeliv
 import {deliveryOptions, getDeliveryOption, calculateDeliveryDate} from '../../data/deliverOptions.js';
 import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
-import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {renderPaymentSummary} from './paymentSummary.js';
 import {renderCheckoutHeader} from './checkoutHeader.js';
 
