3dd7abbe8a49b2ac7768c4229a3fbdcd60df4d22
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2025-11-30 21:14:41 +1000

    13. Amazon - Update tracking bar based on the delivery time

--- 0. JS Basic/13. Amazon-clone/scripts/tracking.js
+++ 0. JS Basic/13. Amazon-clone/scripts/tracking.js
@@ -19,6 +19,10 @@ async function loadPage(){
         }
     });
 
+    const today = dayjs();
+    const orderTime = dayjs(order.orderTime);
+    const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
+    const percentProgress = ((today-orderTime)/(deliveryTime-orderTime))*100;
     
     const trackingHTML = `
         <a class="back-to-orders-link link-primary" href="orders.html">
@@ -26,7 +30,7 @@ async function loadPage(){
         </a>
 
         <div class="delivery-date">
-          Arriving on ${dayjs(productDetails.estimatedDeliveryTime).format('dddd, MMMM D')}
+          Arriving on ${deliveryTime.format('dddd, MMMM D')}
         </div>
 
         <div class="product-info">
@@ -40,19 +44,19 @@ async function loadPage(){
         <img class="product-image" src="${product.image}">
 
         <div class="progress-labels-container">
-          <div class="progress-label">
+          <div class="progress-label ${percentProgress < 50 ? 'current-status' : ''}">
             Preparing
           </div>
-          <div class="progress-label current-status">
+          <div class="progress-label ${percentProgress >= 50 && percentProgress < 100 ? 'current-status' : ''}">
             Shipped
           </div>
-          <div class="progress-label">
+          <div class="progress-label ${percentProgress >= 100 ? 'current-status' : ''}">
             Delivered
           </div>
         </div>
 
         <div class="progress-bar-container">
-          <div class="progress-bar"></div>
+          <div class="progress-bar" style="width:${percentProgress}%"></div>
         </div>
     `;
 
