d1c7cc176bee782714903d8f70fe632e3b133972
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2025-11-30 15:04:11 +1000

    13. Amazon - Convert loadProducts from XHR to fetch

--- 0. JS Basic/13. Amazon-clone/data/products.js
+++ 0. JS Basic/13. Amazon-clone/data/products.js
@@ -74,6 +74,31 @@ export class Appliance extends Product {
 
 export let products=[];
 
+export function loadProductsFetch(){
+  const promise = fetch(
+    'https://supersimplebackend.dev/products'
+
+  ).then((response) => {
+    return response.json()
+  }).then((productsData) => {
+    products = productsData.map((productDetails) => {
+      if (productDetails.type === 'clothing'){
+        return new Clothing(productDetails);
+      } else if (productDetails.type === 'appliance'){
+        return new Appliance(productDetails);
+      } 
+      
+      return new Product(productDetails);
+    });
+    
+    console.log('product loaded');
+    
+  });
+
+  return promise;
+}
+
+/*
 export function loadProducts(fun){
   const xhr = new XMLHttpRequest();
 
@@ -96,6 +121,7 @@ export function loadProducts(fun){
   xhr.setRequestHeader('Cache-Control', 'no-cache');
   xhr.send();
 }
+*/
 
 /*
 export const products = [
--- 0. JS Basic/13. Amazon-clone/scripts/amazon.js
+++ 0. JS Basic/13. Amazon-clone/scripts/amazon.js
@@ -1,7 +1,10 @@
 import {cart, addToCart, calculateCartQuantity} from '../data/cart.js';
-import {products, loadProducts} from '../data/products.js';
+import {products, loadProductsFetch} from '../data/products.js';
 
-loadProducts(renderProductGrid);
+loadProductsFetch()
+  .then(() => {
+      renderProductGrid();
+  });
 
 function renderProductGrid() {
 
