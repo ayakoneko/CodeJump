80f36318d6255eb36a12d6f88b99d146a4ad8423
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2025-12-07 16:43:37 +1000

    REACT 6 - Reorganize the logo/images to src/assets

--- 0. React/ecommerce-project/src/pages/OrdersPage.jsx
+++ 0. React/ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router';
 import { Header } from '../components/Header';
+import BuyAgainIcon from '../assets/images/icons/buy-again.png';
 import "./OrdersPage.css";
 
 export function OrdersPage() {
@@ -47,7 +48,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgainIcon}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
@@ -74,7 +75,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgainIcon}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
@@ -121,7 +122,7 @@ export function OrdersPage() {
                 <button className="buy-again-button button-primary">
                   <img
                     className="buy-again-icon"
-                    src="images/icons/buy-again.png"
+                    src={BuyAgainIcon}
                   />
                   <span className="buy-again-message">Add to Cart</span>
                 </button>
