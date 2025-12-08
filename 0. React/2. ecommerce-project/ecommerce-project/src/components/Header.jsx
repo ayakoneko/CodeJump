e5bc535301589420153f676996fe6fe6e8addee8
Ayako Kaneko <ayako.kaneko1997@gmail.com>
2025-12-09 01:11:12 +1000

    REACT 11 - Convert ecommerce project to TypeScript

---
+++ 0. React/2. ecommerce-project/ecommerce-project/src/components/Header.jsx
@@ -0,0 +1,69 @@
+import { useState } from 'react';
+import { NavLink, useNavigate, useSearchParams } from 'react-router';
+import CartIcon from '../assets/images/icons/cart-icon.png';
+import SearchIcon from '../assets/images/icons/search-icon.png';
+import LogoWhite from '../assets/images/logo-white.png';
+import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
+import "./Header.css";
+
+export function Header({cart}) {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const searchText = searchParams.get('search');
+  const [search, setSearch] = useState(searchText || '');
+
+  const updateSearchInput = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const searchProducts = () => {
+    navigate(`/?search=${search}`);
+  };
+
+  const handleSearchKeyDown = (event) => {
+    const keyPressed = event.key;
+    if (keyPressed === 'Enter'){
+      searchProducts()
+    } else if (keyPressed === 'Escape') {
+      setSearch('');
+      navigate(`/`);
+    }
+  };
+
+  let totalQuantity = 0;
+  cart.forEach((cartItem) => {
+    totalQuantity += cartItem.quantity;
+  })
+
+  return (
+    <div className="header">
+      <div className="left-section">
+        <NavLink to="/" className="header-link">
+          <img className="logo" data-testid="header-logo" src={LogoWhite} />
+          <img className="mobile-logo" data-testid="header-mobile-logo" src={MobileLogoWhite} />
+        </NavLink>
+      </div>
+
+      <div className="middle-section">
+        <input className="search-bar" data-testid="header-search-bar" type="text" placeholder="Search" 
+          value={search} onChange={updateSearchInput} onKeyDown={handleSearchKeyDown}/>
+
+        <button className="search-button" data-testid="header-search-button" onClick={searchProducts}>
+          <img className="search-icon" src={SearchIcon} />
+        </button>
+      </div>
+
+      <div className="right-section">
+        <NavLink className="orders-link header-link" data-testid="header-orders-link" to="/orders">
+          <span className="orders-text">Orders</span>
+        </NavLink>
+
+        <NavLink className="cart-link header-link" data-testid="header-cart-link" to="/checkout">
+          <img className="cart-icon" src={CartIcon} />
+          <div className="cart-quantity">{totalQuantity}</div>
+          <div className="cart-text">Cart</div>
+        </NavLink>
+      </div>
+    </div>
+  );
+}
